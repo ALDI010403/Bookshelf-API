c5731a7ab92ad9cb684b62612c3c74dd4179844b
Aldi <aldi010403@gmail.com>
2023-07-30 15:10:20 +0700

    initial commit

---
+++ src/handler.js
@@ -0,0 +1,350 @@
+const { nanoid } = require('nanoid');
+const books = require('./books');
+
+/*
+    ini adalah handler untuk fungsi menambahkan data buku baru
+*/
+const addBookHandler = (request, h) => {
+    const {
+        name,
+        year,
+        author,
+        summary,
+        publisher,
+        pageCount,
+        readPage,
+        reading
+    } = request.payload;
+
+    /**
+        Client tidak melampirkan properti name pada request body
+        maka server akan merespon status code = 400
+        Response body = 
+        {
+        "status": "fail",
+        "message": "Gagal menambahkan buku. Mohon isi nama buku"
+        }
+    */
+
+    if (!name) {
+        const response = h.response({
+            status: 'fail',
+            message: 'Gagal menambahkan buku. Mohon isi nama buku'
+        });
+
+        response.code(400);
+        return response;
+    }
+
+    /**
+        Client melampirkan nilai properti readPage yang lebih besar dari nilai properti pageCount. Bila hal ini terjadi, maka server akan merespons dengan:
+        Status Code : 400
+        Response Body:
+        {
+        "status": "fail",
+        "message": "Gagal menambahkan buku. readPage tidak boleh lebih besar dari pageCount"
+        }
+    */
+
+    if (readPage > pageCount) {
+        const response = h.response({
+            status: 'fail',
+            message: 'Gagal menambahkan buku. readPage tidak boleh lebih besar dari pageCount'
+        });
+
+        response.code(400);
+        return response;
+    }
+
+    const id = nanoid(16);
+    const insertedAt = new Date().toISOString();
+    const updatedAt = insertedAt;
+    const finished = pageCount === readPage;
+
+    const newBook = {
+        id,
+        name,
+        year,
+        author,
+        summary,
+        publisher,
+        pageCount,
+        readPage,
+        finished,
+        reading,
+        insertedAt,
+        updatedAt
+    };
+
+    books.push(newBook);
+    const isSuccess = books.filter((book) => book.id === id).length > 0;
+
+    /**
+        Bila buku berhasil dimasukkan, server harus mengembalikan respons dengan:
+
+        Status Code : 201
+        Response Body:
+        {
+            "status": "success",
+            "message": "Buku berhasil ditambahkan",
+            "data": {
+                "bookId": "1L7ZtDUFeGs7VlEt"
+            }
+        }
+    */
+
+    if (isSuccess) {
+        const response = h.response({
+            status: 'success',
+            message: 'Buku berhasil ditambahkan',
+            data: {
+                bookId: id
+            }
+        });
+
+        response.code(201);
+        return response;
+    }
+};
+
+/*
+    ini adalah handler untuk menampilkan data buku
+*/
+const getAllBooksHandler = (request, h) => {
+    const {
+        name,
+        reading,
+        finished
+    } = request.query;
+
+    let filteredBooks = books;
+
+    if (name) {
+        filteredBooks = books.filter((bn) => bn.name.toLowerCase().includes(name.toLowerCase()));
+    }
+
+    if (reading) {
+        filteredBooks = books.filter((book) => Number(book.reading) === Number(reading));
+    }
+
+    if (finished) {
+        filteredBooks = books.filter((book) => Number(book.finished) === Number(finished));
+    }
+
+    const response = h.response({
+        status: 'success',
+        data: {
+            books: filteredBooks.map((book) => ({
+                id: book.id,
+                name: book.name,
+                publisher: book.publisher
+            }))
+        }
+    });
+
+    response.code(200);
+    return response;
+};
+
+const getBookByIdHandler = (request, h) => {
+    const {
+        id
+    } = request.params;
+
+    const book = books.filter((b) => b.id === id)[0];
+
+    /**
+        ketika buku dengan id di temukan 
+    */
+
+    if (book !== undefined) {
+        return {
+            status: 'success',
+            data: {
+                book
+            }
+        };
+    }
+
+    /**
+       ketika buku dengan id yang tidak di temukan di server
+    */
+
+    const response = h.response({
+        status: 'fail',
+        message: 'Buku tidak ditemukan'
+    });
+
+    response.code(404);
+    return response;
+};
+
+/*
+    ini adalah handler untuk mengedit buku
+*/
+const editBookByIdHandler = (request, h) => {
+    const { id } = request.params;
+
+    const {
+        name,
+        year,
+        author,
+        summary,
+        publisher,
+        pageCount,
+        readPage,
+        reading
+    } = request.payload;
+
+    const updatedAt = new Date().toISOString();
+    const index = books.findIndex((book) => book.id === id);
+
+    /*
+       Client tidak melampirkan properti name pada request body. Bila hal ini terjadi, maka server akan merespons dengan:
+        Status Code : 400
+        Response Body:
+        {
+        "status": "fail",
+        "message": "Gagal memperbarui buku. Mohon isi nama buku"
+        }
+    */
+
+    if (!name) {
+        const response = h.response({
+            status: 'fail',
+            message: 'Gagal memperbarui buku. Mohon isi nama buku'
+        });
+        response.code(400);
+
+        return response;
+    }
+
+    /**
+        Client melampirkan nilai properti readPage yang lebih besar dari nilai properti pageCount. Bila hal ini terjadi, maka server akan merespons dengan:
+            Status Code : 400
+            Response Body:
+            {
+            "status": "fail",
+            "message": "Gagal memperbarui buku. readPage tidak boleh lebih besar dari pageCount"
+            }
+
+    */
+
+    if (readPage > pageCount) {
+        const response = h.response({
+            status: 'fail',
+            message: 'Gagal memperbarui buku. readPage tidak boleh lebih besar dari pageCount'
+        });
+        response.code(400);
+
+        return response;
+    }
+
+    if (index !== -1) {
+        const finished = pageCount === readPage;
+
+        books[index] = {
+            ...books[index],
+            name,
+            year,
+            author,
+            summary,
+            publisher,
+            pageCount,
+            readPage,
+            finished,
+            reading,
+            updatedAt
+        };
+
+        /**
+            Ketika Buku berhasil diperbarui.
+        */
+
+        const response = h.response({
+            status: 'success',
+            message: 'Buku berhasil diperbarui'
+        });
+
+        response.code(200);
+        return response;
+    }
+
+    /**
+        ketika Id yang dilampirkan oleh client tidak ditemukkan oleh server. Bila hal ini terjadi, maka server akan merespons dengan:
+        Status Code : 404
+        Response Body:
+        {
+        "status": "fail",
+        "message": "Gagal memperbarui buku. Id tidak ditemukan"
+        }
+    */
+
+    const response = h.response({
+        status: 'fail',
+        message: 'Gagal memperbarui buku. Id tidak ditemukan'
+    });
+
+    response.code(404);
+    return response;
+};
+
+/*
+    ini adalah handler untuk menghapus buku
+*/
+const deleteBookByIdHandler = (request, h) => {
+    const {
+        id
+    } = request.params;
+
+    const index = books.findIndex((note) => note.id === id);
+
+    /*
+       Bila id yang dilampirkan tidak dimiliki oleh buku manapun, maka server harus mengembalikan respons berikut:
+
+        Status Code : 404
+        Response Body:
+        {
+        "status": "fail",
+        "message": "Buku gagal dihapus. Id tidak ditemukan"
+        }
+    */
+
+    if (index !== -1) {
+        books.splice(index, 1);
+        const response = h.response({
+            status: 'success',
+            message: 'Buku berhasil dihapus'
+        });
+
+        response.code(200);
+        return response;
+    }
+
+    /*
+       Bila id yang dilampirkan tidak dimiliki oleh buku manapun, maka server harus mengembalikan respons berikut:
+
+        Status Code : 404
+        Response Body:
+        {
+        "status": "success",
+        "message": "Buku berhasil dihapus"
+        }
+    */
+
+    const response = h.response({
+        status: 'fail',
+        message: 'Buku gagal dihapus. Id tidak ditemukan'
+    });
+
+    response.code(404);
+    return response;
+};
+
+module.exports = {
+    addBookHandler,
+    getAllBooksHandler,
+    getBookByIdHandler,
+    editBookByIdHandler,
+    deleteBookByIdHandler
+};
